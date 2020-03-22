dc5dec63fd003837799d2027c5271dba5c52575d
Matthew Wright <matthew.wright5@gmail.com>
2020-03-22 15:22:08 +1100

    finish working and refactored palindrome method

--- lib/wrytee_palindrome.rb
+++ lib/wrytee_palindrome.rb
@@ -1,6 +1,22 @@
 require "wrytee_palindrome/version"
 
-module WryteePalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  # Returns only the letters in a string.
+  def letters
+    self.chars.select { |c| c.match(/[a-z]/i) }.join
+  end
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      self.letters.downcase
+    end
 end
+
--- test/wrytee_palindrome_test.rb
+++ test/wrytee_palindrome_test.rb
@@ -1,11 +1,25 @@
 require "test_helper"
 
 class WryteePalindromeTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::WryteePalindrome::VERSION
+
+  def test_non_palindrome
+    refute "apple".palindrome?
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_literal_palindrome
+    assert "racecar".palindrome?
   end
+
+  def test_mixed_case_palindrome
+    assert "RaceCar".palindrome?
+  end
+
+  def test_palindrome_with_punctuation
+    assert "Madam, I'm Adam.".palindrome?
+  end
+
+  def test_letters
+    assert_equal "MadamImAdam", "Madam, I'm Adam.".letters
+  end
+
 end
